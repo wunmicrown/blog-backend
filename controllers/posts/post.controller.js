074c0e3eb563c7421158893b08d5f58d7143456e
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-03-21 18:31:27 +0100

    worked on postController file. At crate post function

--- controllers/posts/post.controller.js
+++ controllers/posts/post.controller.js
@@ -3,22 +3,49 @@ const { excludeFields } = require("../../utils/common.methods");
 const { cloudDelete } = require("../../utils/cloudinary.utils");
 const User = require("../../model/user/user.model");
 const Post = require("../../model/post/post.model");
+const Category = require("../../model/category/category.model");
 require('fs').promises;
 
 const postController ={
 
  //!Create post
  createPost: asyncHandler(async (req, res) => {
-  const { title, description } = req.body;
-  console.log("title",title, "description",description);
-  const user = await User.findById(req.auth_id);
-  const post = await Post.create({
+    //get the payload
+  const { title, description,category } = req.body;
+  console.log("title:",title, "description:",description, "category:",category);
+  // find the category
+  const categoryFound = await Category.findById(category);
+  if (!categoryFound) {
+    throw new Error("Category not found");
+  }
+   // find the user
+   const userFound = await User.findById(req.user);
+   if (!userFound) {
+     throw new Error("User not found");
+   }
+
+  // create the post
+  const postCreated = await Post.create({
     title,
     description,
-    user: user._id,
-  });
-  return res.status(200).json({ message: 'Post created successfully', post });
+    category,
+    image: req.file.path,
+  })
+
+   // the post was pushed into category
+   categoryFound.posts.push(categoryFound?._id);
+   //resave the category
+   await categoryFound.save();
+   //push the posts into user
+   userFound.posts.push(postCreated?._id);
+   //Update the user account type
+   userFound.updateAccountType();
+   //save the user
+   await userFound.save();
+   //send the post to the client
+   return res.status(200).json({status: "success", message: 'Post created successfully', post: postCreated });
  }),
+
 
  uploads: asyncHandler(async (req, res) => {
   if (!req.file || Object.keys(req.file).length === 0) {
