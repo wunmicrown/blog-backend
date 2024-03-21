ef4850825f5f9d6efd4efbc5aa9b98e9399cb618
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-03-21 14:09:25 +0100

    Added uploads function where user can uplod their pofile pic. make used of cloudinary to store the images.

--- controllers/posts/post.controller.js
+++ controllers/posts/post.controller.js
@@ -8,35 +8,32 @@ const postController ={
 
  //!Create post
  createPost: asyncHandler(async (req, res) => {}),
- uploads:asyncHandler(async (req, res) => {
-    try {
-        //req.file for single file : req.files for multiple files [array of object]
-        if (!req.file || Object.keys(req.file).length === 0) {
-          return res.status(400).json({ message: 'No files were uploaded.' });
-        }
-    
-        const user = await User.findById(req.auth_id);
-        console.log("User:",user);
-        oldPic = user.profilePic ?? null;
-        if (oldPic) {
-    
-          // Below for cloudinary deletion
-          let id = oldPic.split('/').pop().split('.')[0];
-          const { status, error } = await cloudDelete(id);
-          if (!status) console.log(error);
-        }
-    
-        // Using (multer-storage-cloudinary) i.e not pre-saving file to local disk
-        user.profilePic = req.file.path;
-        user.save();
-        const _user = excludeFields(user.toObject(), ["otp", "password", "__v"])
-        return res.status(200).json({ user: _user })
-    
-      } catch (error) {
-        console.log(error);
-        return res.status(500).json({ message: "Internal server error" });
-      }
- }),
+ uploads: asyncHandler(async (req, res) => {
+  if (!req.file || Object.keys(req.file).length === 0) {
+    return res.status(400).json({ message: 'No files were uploaded.' });
+  }
+
+  const user = await User.findById(req.auth_id);
+  console.log("User:", user);
+  const oldPic = user.profilePic ?? null;
+  if (oldPic) {
+    // Below for cloudinary deletion
+    let id = oldPic.split('/').pop().split('.')[0];
+    const { status, error } = await cloudDelete(id);
+    if (!status) {
+      console.log(error);
+      return res.status(500).json({ message: 'Failed to delete old image.' });
+    }
+  }
+
+  // Using (multer-storage-cloudinary) i.e not pre-saving file to local disk
+  user.profilePic = req.file.path;
+  await user.save(); // Await the save operation
+
+  const _user = excludeFields(user.toObject(), ["otp", "password", "__v"]);
+  return res.status(200).json({ message: 'Image uploaded successfully', user: _user });
+}),
+
 
 };
 
