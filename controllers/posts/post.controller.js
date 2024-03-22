23ddc2978b44886c5af328db9fffc9facaccb9b7
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-03-22 17:27:24 +0100

    added a fetchAllPosts function. That contians all the process how to fecth all posted by users

--- controllers/posts/post.controller.js
+++ controllers/posts/post.controller.js
@@ -80,7 +80,34 @@ console.log("postCreated",postCreated)
   });
  }),
 
- 
+ //!list all posts
+ fetchAllPosts: asyncHandler(async (req, res) => {
+  const { category, title, page = 1, limit = 300 } = req.query;
+  //Basic filter
+  let filter = {};
+  if (category) {
+    filter.category = category;
+  }
+  if (title) {
+    filter.description = { $regex: title, $options: "i" }; //case insensitive
+  }
+
+  const posts = await Post.find(filter)
+    .populate("category")
+    .sort({ createdAt: -1 })
+    .skip((page - 1) * limit)
+    .limit(limit);
+  //total posts
+  const totalPosts = await Post.countDocuments(filter);
+  res.json({
+    status: "success",
+    message: "Post fetched successfully",
+    posts,
+    currentPage: page,
+    perPage: limit,
+    totalPages: Math.ceil(totalPosts / limit),
+  });
+}),
 };
 
 
