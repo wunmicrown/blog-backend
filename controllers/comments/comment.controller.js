bdcdc0abb161ba5f8bc660f20081530e1fa4e4ff
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-03-23 07:31:47 +0100

    Added a postId and content coming from the users

--- controllers/comments/comment.controller.js
+++ controllers/comments/comment.controller.js
@@ -3,7 +3,9 @@ const asyncHandler = require("express-async-handler");
 const commentsController = {
     // Create comments
     createComment: asyncHandler(async (req, res) => {
-        
+        // . Find the post Id
+        const {postId, content}= req.body;
+        // . Find the post
     })
 };
 
