d5a00fa2fda5054952ad56d5040ea5cc68dc2472
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-03-23 07:37:16 +0100

    Chnages were made at  createComment function

--- controllers/comments/comment.controller.js
+++ controllers/comments/comment.controller.js
@@ -1,11 +1,33 @@
 const asyncHandler = require("express-async-handler");
+const Post = require("../../model/post/post.model");
 
 const commentsController = {
     // Create comments
     createComment: asyncHandler(async (req, res) => {
         // . Find the post Id
-        const {postId, content}= req.body;
+        const { postId, content } = req.body;
         // . Find the post
+        const post = await Post.findById(postId);
+        if (!post) {
+            throw new Error("Post not found");
+        }
+        //. Create the comment
+        const commentCreated = await Comment.create({
+            content,
+            author: req.auth_id,
+            postId
+        });
+
+        //. Push the comment to the post
+        post.comments.push(commentCreated?._id);
+        await post.save();
+
+        //. Send the response
+        res.json({
+            status: "success",
+            message: "Comment created successfully",
+            commentCreated,
+        });
     })
 };
 
