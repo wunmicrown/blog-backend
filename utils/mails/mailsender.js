83a353d7d80bebea65c33daeb0721be101696ee5
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-03-23 08:16:59 +0100

    Reformatted code for better readability and organization

--- utils/mails/mailsender.js
+++ utils/mails/mailsender.js
@@ -4,51 +4,50 @@ const { otpEmailTemplate, } = require("../otpTemplate");
 
 // global varibale for genarating OTP
 const generateSixDigitNumber = () => {
-    return Math.floor(Math.random() * 900000) + 100000;
-  };
-  
+  return Math.floor(Math.random() * 900000) + 100000;
+};
+
 /***
  * 
  */
 const sendOtpEmail = async (email, username) => {
-    try {
+  try {
     // Generate an OTP for email verification
-    const otpGen =generateSixDigitNumber()
+    const otpGen = generateSixDigitNumber()
     // Create SMTP transporter
-        let transporter = nodemailer.createTransport({
-            host: process.env.MAIL_HOST,
-            port: process.env.MAIL_PORT,
-            secure: process.env.MAIL_USE_TLS,
-            auth: {
-            user: process.env.MAIL_USER,
-            pass: process.env.MAIL_PASS,
-            },
-      });
-  
-      // Email messege
-      const mailOptions = {
-        from: `"Blogify" ${process.env.MAIL_USER}`,
-        to: email,
-        subject: "OTP Verification",
-        html: otpEmailTemplate(username, otpGen)
+    let transporter = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
+      port: process.env.MAIL_PORT,
+      secure: process.env.MAIL_USE_TLS,
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
+      },
+    });
+
+    // Email messege
+    const mailOptions = {
+      from: `"Blogify" ${process.env.MAIL_USER}`,
+      to: email,
+      subject: "OTP Verification",
+      html: otpEmailTemplate(username, otpGen)
     };
 
     //   Send email
     await transporter.sendMail(mailOptions);
-      // Save OTP in database
-      await User.updateOne({ email: email }, { $set: { otp: otpGen } });
-  
-      return otpGen;
-    } catch (error) {
-      console.error("Error sending OTP:", error);
-      throw new Error("Error sending OTP");
-    }
-  };
+    // Save OTP in database
+    await User.updateOne({ email: email }, { $set: { otp: otpGen } });
 
-  module.exports = {
-    sendOtpEmail,
-    
+    return otpGen;
+  } catch (error) {
+    console.error("Error sending OTP:", error);
+    throw new Error("Error sending OTP");
   }
-  
- 
- 
+};
+
+module.exports = {
+  sendOtpEmail,
+
+}
+
+
