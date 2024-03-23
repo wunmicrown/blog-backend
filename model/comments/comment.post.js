36c5458e431c88ecb2a5eb57331562b1a340c34e
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-03-23 07:04:47 +0100

    Changes were made to the commmentSchema model

--- model/comments/comment.post.js
+++ model/comments/comment.post.js
@@ -2,10 +2,22 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const CommentSchema = new Schema({
-  username: { type: String, required: true, maxlength: 30 },
-  content: { type: String, required: true },
-  postId: { type: String, required: true },
-  timestamp: { type: Date },
-});
-
-module.exports = mongoose.model("Comment", CommentSchema);
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  post: {
+    type: Schema.Types.ObjectId,
+    ref: "Post",
+    required: true,
+  },
+},
+  { timestamps: true }
+);
+let Comment = mongoose.model("Comment", CommentSchema);
+module.exports = Comment;
