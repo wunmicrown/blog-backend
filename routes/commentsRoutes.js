b41275d4fe1d6bff1b19b95f6c3deb86b4fdf68e
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-03-23 07:16:29 +0100

    commentRoutes.js setup

--- routes/commentsRoutes.js
+++ routes/commentsRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const { TOKEN_MIDDLEWARE } = require('../middleWares/authenticateToken');
 
 
 // Create instance of express router
 const commentRoutes=express.Router();
 
 // ------Create comment------
-
+commentRoutes.post("/create-comment", TOKEN_MIDDLEWARE)
 
 module.exports=commentRoutes;
