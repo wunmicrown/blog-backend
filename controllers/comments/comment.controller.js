1988ddd52bcee28b4166ae10832aa8b0ba5fffb8
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-03-23 07:26:35 +0100

    createComment function was added

--- controllers/comments/comment.controller.js
+++ controllers/comments/comment.controller.js
@@ -2,7 +2,9 @@ const asyncHandler = require("express-async-handler");
 
 const commentsController = {
     // Create comments
-    
+    createComment: asyncHandler(async (req, res) => {
+        
+    })
 };
 
 module.exports = commentsController;
