cedc8d395e2b0069260b2294a81aa36dcc57c567
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-03-23 07:25:22 +0100

    setup commentController js

---
+++ controllers/comments/comment.controller.js
@@ -0,0 +1,8 @@
+const asyncHandler = require("express-async-handler");
+
+const commentsController = {
+    // Create comments
+    
+};
+
+module.exports = commentsController;
--- routes/commentsRoutes.js
+++ routes/commentsRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { TOKEN_MIDDLEWARE } = require('../middleWares/authenticateToken');
+const commentsController = require('../controllers/comments/comment.controller');
 
 
 // Create instance of express router
 const commentRoutes=express.Router();
 
 // ------Create comment------
-commentRoutes.post("/create-comment", TOKEN_MIDDLEWARE)
+commentRoutes.post("/create-comment", TOKEN_MIDDLEWARE, commentsController.createComment)
 
 module.exports=commentRoutes;
